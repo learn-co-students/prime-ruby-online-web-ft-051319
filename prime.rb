2f8b13a477f4126aedc0b1c3231cdd2fdb854494
M Banks <montensia1@outlook.com>
2019-05-20 21:27:15 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  #all of the whole number multiples are NOT prime numbers.  The others are prime numbers
+  if num < 2
+    return false 
+  elsif num == 2 
+    return true 
+  else
+      counter = 2 
+      while counter < num
+        if num % counter == 0
+          return false 
+          counter = counter + 1
+        end
+      end
+  end
+  return true
+end
